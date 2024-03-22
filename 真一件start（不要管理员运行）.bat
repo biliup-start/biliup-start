3cb04c01ef711416d799f07914e0523d24c23623
ikun1993 <96544807+ikun1993@users.noreply.github.com>
2024-03-22 14:47:36 +0800

    Add files via upload
    win
    和
    debian

---
+++ 真一件start（不要管理员运行）.bat
@@ -0,0 +1,188 @@
+@echo off
+
+:inputDrive
+set UserDrive=
+set /p UserDrive="请输入你想录播的盘符（默认为C盘）："
+if "%UserDrive%"=="" (
+    set UserDrive=C
+)
+echo %UserDrive%| findstr /R "^[a-zA-Z]*$" > nul
+if %errorlevel%==1 (
+    echo 错误: 你输入的不是字母，请重新输入。
+    goto inputDrive
+)
+if not exist %UserDrive%:\ (
+    echo 错误: 没有找到你选择的 %UserDrive%盘 请到我的电脑中查看正确盘符
+    goto inputDrive
+)
+echo 你录播文件和日志在 %UserDrive%:\opt\biliup
+
+where python >nul 2>nul
+if %errorlevel% neq 0 (
+    echo 未安装 python
+    set biliversion=未安装biliup
+    set pipversion=未安装python
+    goto end
+)
+
+echo 检查biliup版本...
+for /f "tokens=2 delims= " %%i in ('pip3 show biliup ^| findstr Version') do set biliversion=%%i
+for /f "tokens=2 delims= " %%i in ('yolk -V biliup 2^>nul') do set pipversion=%%i
+
+if not defined pipversion (
+    echo 查询最新版本失败，正在尝试安装 yolk3k...
+    powershell -Command "Start-Process -FilePath 'pip3' -ArgumentList 'install yolk3k' -Verb RunAs -Wait"
+    for /f "tokens=2 delims= " %%i in ('yolk -V biliup 2^>nul') do set pipversion=%%i
+    if not defined pipversion (
+        echo 安装 yolk3k失败  跳过自动更新 手动更新终端如数 pip3 install -U biliup ...
+        set pipversion=%biliversion%
+    )
+) 
+
+if "%pipversion%"=="%biliversion%" (
+    echo 当前运行版本 v%biliversion%
+) else (
+    echo 当前最新版本 v%pipversion%
+)
+
+:end
+if not defined biliversion (
+    echo 未运行过脚本 开始执行安装
+    echo 正在创建运行目录 %UserDrive%:\opt\biliup...
+    mkdir %UserDrive%:\opt\biliup
+
+    echo 删除可能存在的 chocolatey 目录...
+    if exist C:\opt\biliup\biliupR-v0.1.19-x86_64-windows (
+        powershell -Command "Start-Process -FilePath 'cmd.exe' -ArgumentList '/c rmdir /s /q C:\ProgramData\chocolatey' -Verb RunAs"
+        echo 删除 biliupR-v0.1.19-x86_64-windows 目录成功
+    )
+
+    echo 检查 windowsbiliup.bat 是否存在 如存在进行下一步...
+    if not exist %~dp0\windowsbiliup.bat (
+        echo 正在下载 windowsbiliup.bat...
+        powershell -Command "Invoke-WebRequest -Uri 'https://blrec.iokun.top/d/189/windowsbiliup.bat?sign=WFofEtv2yAG3tO4WvAJCaWiLzHx1elLMdTMCPkP_y70=:0' -OutFile 'windowsbiliup.bat'"
+    )
+
+    echo 以管理员身份运行 windowsbiliup.bat...
+    powershell -Command "Start-Process -FilePath 'windowsbiliup.bat' -Verb RunAs -Wait"
+
+    echo 检查 biliupR-v0.1.19-x86_64-windows.zip 是否存在 如存在进行下一步...
+    if not exist %~dp0\biliupR-v0.1.19-x86_64-windows.zip (
+        echo 正在下载 biliupR-v0.1.19-x86_64-windows.zip...
+        powershell -Command "Invoke-WebRequest -Uri 'https://j.iokun.top/https://github.com/biliup/biliup-rs/releases/download/v0.1.19/biliupR-v0.1.19-x86_64-windows.zip' -OutFile 'biliupR-v0.1.19-x86_64-windows.zip'"
+    )
+
+    echo 正在将 biliupR-v0.1.19-x86_64-windows.zip 解压到 %UserDrive%:\opt\biliup...
+    powershell -Command "Expand-Archive -Path '%~dp0\biliupR-v0.1.19-x86_64-windows.zip' -DestinationPath '%UserDrive%:\opt\biliup' -Force"
+    powershell -Command "Move-Item -Path '%UserDrive%:\opt\biliup\biliupR-v0.1.19-x86_64-windows\biliup.exe' -Destination '%UserDrive%:\opt\biliup\biliupR.exe'"
+
+    echo 删除可能存在的 windowsbiliup.bat...
+    if exist %~dp0\windowsbiliup.bat (
+        powershell -Command "Start-Process -FilePath 'cmd.exe' -ArgumentList '/c del %~dp0\windowsbiliup.bat' -Verb RunAs"
+        echo 删除 windowsbiliup.bat成功
+    )
+
+    echo 删除可能存在的 biliupR-v0.1.19-x86_64-windows.zip...
+    if exist %~dp0\biliupR-v0.1.19-x86_64-windows.zip (
+        powershell -Command "Start-Process -FilePath 'cmd.exe' -ArgumentList '/c del %~dp0\biliupR-v0.1.19-x86_64-windows.zip' -Verb RunAs"
+        echo 删除 biliupR-v0.1.19-x86_64-windows.zip成功
+    )
+
+    echo 删除可能存在的 biliupR-v0.1.19-x86_64-windows 目录...
+    if exist %UserDrive%:\opt\biliup\biliupR-v0.1.19-x86_64-windows (
+        powershell -Command "Start-Process -FilePath 'cmd.exe' -ArgumentList '/c rmdir /s /q %UserDrive%:\opt\biliup\biliupR-v0.1.19-x86_64-windows' -Verb RunAs"
+        echo 删除 biliupR-v0.1.19-x86_64-windows 目录成功
+    )
+)
+echo 查询库中可用版本 如最新跳过...
+if not "%pipversion%"=="%biliversion%" (
+    set /p UserUpdate="biliup版本过低，强制更新? [如不需要请关闭停用脚本]："
+    if "%UserUpdate%"=="" (
+        powershell -Command "Start-Process -FilePath 'pip3' -ArgumentList 'install -U biliup' -Verb RunAs -Wait"
+        echo 已更新到版本 v%pipversion% 
+    ) else (
+        echo 更新失败 v%pipversion% ，请手动更新
+    )
+) 
+
+echo 检查 cookies.json 是否存在（B站是否登录）...
+if not exist %UserDrive%:\opt\biliup\cookies.json (
+    echo cookies.json 不存在正在登录B站（推荐扫码）...
+    cd %UserDrive%:\opt\biliup
+    .\biliupR.exe login
+)
+
+timeout /t 3 /nobreak >nul
+
+if exist %UserDrive%:\opt\biliup\cookies.json (
+    if exist %UserDrive%:\opt\biliup\qrcode.png (
+        powershell -Command "Start-Process -FilePath 'cmd.exe' -ArgumentList '/c del %UserDrive%:\opt\biliup\qrcode.png' -Verb RunAs"    
+        echo 登录成功 删除登录二维码图片
+    )else (
+        echo 登录成功或 cookies.json 文件已存在
+    )
+) else (
+    echo 登录失败 请打开终端输入 %UserDrive%:\opt\biliup\biliupR.exe login 手动登录
+)
+
+timeout /t 5 /nobreak >nul
+
+setlocal enabledelayedexpansion
+set "ForbiddenPorts=0 1 7 9 11 13 15 17 19 20 21 22 23 25 37 42 43 53 77 79 87 95 101 102 103 104 109 110 111 113 115 117 119 123 135 139 143 179 389 465 512 513 514 515 526 530 531 532 540 556 563 587 601 636 993 995 2049 3659 4045 6000 6665 6666 6667 6668 6669 137 139 445 593 1025 2745 3127 6129 3389"
+:input
+set UserInput=
+set /p UserInput="请输入一个小于65535端口(回车默认19159)："
+if "%UserInput%"=="" (
+    set UserInput=19159
+)
+echo %UserInput%| findstr /R "^[0-9][0-9]*$" > nul
+if %errorlevel%==1 (
+    echo 错误: 你输入的不是数字，请重新输入。
+    goto input
+)
+for %%i in (%ForbiddenPorts%) do (
+    if %UserInput% equ %%i (
+        echo 错误: 端口 %UserInput% 被禁用，请重新输入。
+        goto input
+    )
+)
+set num=%UserInput%
+set len=0
+:loop
+if defined num (
+    set /A len+=1
+    set num=%num:~1%
+    goto loop
+)
+if %len% GTR 5 (
+    echo 错误: 你输入的数字超过了5位，请重新输入。
+    goto input
+)
+if %UserInput% GTR 65535 (
+    echo 错误: 你输入的数字超过了65535，请重新输入。
+    goto input
+)
+netstat -aon | findstr /R /C:"^  TCP    [0-9.]*:%UserInput% " >nul
+if %errorlevel%==0 (
+    echo 错误: 端口 %UserInput% 已被占用，请重新输入。
+    goto input
+)
+echo 你输入的端口是 %UserInput%
+
+set /p UserPassword="请输入密码(回车不使用密码)："
+if "%UserPassword%"=="" (
+    echo 未启用密码公网不推荐 持续运行biliup需保持当前窗口存在
+)
+
+echo 正在启动biliup 运行成功后10秒自动为你打开webui配置端...
+cd %UserDrive%:\opt\biliup
+if "%UserPassword%"=="" (
+    start /B biliup -P %UserInput% restart
+    timeout /t 11 /nobreak >nul
+    start http://localhost:%UserInput%
+) else (
+    echo 账号：biliup 密码：%UserPassword% 持续运行biliup需保持当前窗口存在
+    start /B biliup -P %UserInput% --password %UserPassword% restart
+    timeout /t 11 /nobreak >nul
+    start http://localhost:%UserInput%
+)
---
+++ 真一件start（需要管理员运行）.sh
@@ -0,0 +1,267 @@
+#!/bin/bash
+
+# 定义颜色代码
+green='\033[0;32m'
+plain='\033[0m'
+red='\e[31m'
+yellow='\e[33m'
+
+# 安装下载命令
+install_biliup() {
+    if [ ! -f "/opt/biliup/install.sh" ]; then
+        sudo bash -c "wget -O /opt/biliup/install.sh https://image.biliup.me/install.sh && chmod +x /opt/biliup/install.sh && bash /opt/biliup/install.sh"
+        echo -e "biliup完成：${green}安装命令已经运行${plain}"
+    else
+        echo -e "biliup完成：${green}安装命令已经存在${plain}"
+    fi
+
+    if ! find /opt/biliup/ -name "*-linux.tar.xz" -print -quit | grep -q .; then
+        echo "你的CPU架构是："
+        echo -e "    （${red}默认${plain}）0: ${yellow}x86_64${plain}"
+        echo -e "            Y: ${green}ARMx64${plain}"
+        echo -e "            N: ${green} ARM${plain}"
+        read -p "请输入[0/Y/N]：" arch_choice
+        if [[ -z "$arch_choice" || ! "$arch_choice" =~ [0-2] ]]; then
+            arch_choice=0
+        fi
+        if [ "$arch_choice" -eq 0 ]; then
+            cd /opt/biliup && wget https://j.iokun.top/https://github.com/biliup/biliup-rs/releases/download/v0.1.19/biliupR-v0.1.19-x86_64-linux.tar.xz && tar -xf biliupR-v0.1.19-x86_64-linux.tar.xz && mv "/opt/biliup/biliupR-v0.1.19-x86_64-linux/biliup" "/opt/biliup/biliupR"
+        elif [ "$arch_choice" -eq 1 ]; then
+            cd /opt/biliup && wget https://j.iokun.top/https://github.com/biliup/biliup-rs/releases/download/v0.1.19/biliupR-v0.1.19-aarch64-linux.tar.xz && tar -xf biliupR-v0.1.19-aarch64-linux.tar.xz && mv "/opt/biliup/biliupR-v0.1.19-aarch64-linux/biliup" "/opt/biliup/biliupR"
+        else
+            cd /opt/biliup && wget https://j.iokun.top/https://github.com/biliup/biliup-rs/releases/download/v0.1.19/biliupR-v0.1.19-arm-linux.tar.xz && tar -xf biliupR-v0.1.19-arm-linux.tar.xz && mv "/opt/biliup/biliupR-v0.1.19-arm-linux/biliup" "/opt/biliup/biliupR"
+        fi
+        echo -e "biliup-rs完成：${green}已经下载${plain}"
+    else
+        echo -e "biliup-rs完成：${green}已经存在${plain}"
+    fi
+}
+
+# 检查/opt/biliup是否存在，如果不存在则创建
+if [ ! -d "/opt/biliup" ]; then
+    mkdir /opt/biliup
+fi
+
+echo -e "录播文件和日志储存在 ${green}/opt/biliup${plain}"
+
+# 检查biliup是否正在运行
+if pgrep -f "biliup" > /dev/null; then
+    read -p "biliup 已安装 你希望重新安装biliup吗？[Y/N]："  rerun
+    if [ -z "$rerun" ]; then
+        rerun=0
+    fi
+    if [ "$rerun" = "y" ]; then
+        pkill -f "biliup" ; rm -f "/opt/biliup/watch_process.pid" 
+        echo -e "${green}已经杀死biliup程，将重新运行biliup${plain}"
+        install_biliup
+    else
+        echo -e "${red}取消重新启动biliup${plain}"
+        read -p "biliup 已运行 你希望新增一个biliup进程吗？[Y/N]："  addnew
+        if [ "$addnew" = "y" ]; then
+            pkill -f "biliup" ; rm -f "/opt/biliup/watch_process.pid" 
+            echo -e "将${green}新增${plain}一个biliup进程"
+        else
+            echo -e "${red}退出脚本${plain}"
+            exit 1
+        fi
+    fi
+else
+    install_biliup
+fi
+
+# 运行前置条件查询python版本
+python_version=$(python3 --version | cut -d " " -f2)
+
+# 使用sort命令和版本比较来决定使用哪个pip命令
+if printf '3.11\n%s' "$python_version" | sort -V | head -n1 | grep -q '3.11'; then
+    echo -e "Python3的版本是${yellow} $python_version ${plain}, 使用${green}pip install --break-system-packages${plain} 来安装..."
+    pip_install_cmd="pip3 install --break-system-packages"
+    python3_install_cmd="--break-system-packages"
+else
+    echo -e "Python3的版本是${yellow} $python_version ${plain}, 使用标准${green} pip install ${plain}来安装..."
+    pip_install_cmd="pip3 install"
+    python3_install_cmd=""
+fi
+
+echo -n "检查biliup版本中，请等待"
+for i in {1..3}
+do
+    echo -n "."
+    sleep 1
+done
+echo ""
+
+# 检查pip3版本并获取biliup的官方版本
+if ! pip3 --version &> /dev/null; then
+    curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py" && sudo python3 "get-pip.py" $python3_install_cmd &&  rm -f "get-pip.py"
+    echo -e "检测到只安装python3 已自动安装${green}最新版本pip3${plain}"   
+else
+    if [[ $(echo "$(pip3 --version | awk '{print $2}') 21.3" | awk '{print ($1 < $2)}') -ne 0 ]]; then
+        official_version=$(pip search biliup | cut -d ' ' -f 2)
+    else
+        if [ -n "$(pip3 show yolk3k | grep Version | cut -d ' ' -f 2)" ]; then
+            official_version=$(timeout 22 yolk -V biliup | cut -d ' ' -f 2)
+        else    
+            sudo $pip_install_cmd yolk3k
+            source ~/.bashrc  # 更新你的 shell
+            official_version=$(timeout 22 yolk -V biliup | cut -d ' ' -f 2)
+        fi
+    fi
+fi
+
+# 检查本地biliup版本
+local_version=$(pip3 show biliup | grep Version | cut -d ' ' -f 2)
+
+echo -e "本地版本：${green} $local_version ${plain}"
+if [ -n "$official_version" ]; then
+    echo -e "最新版本：${yellow} $official_version ${plain}"
+else
+    echo -e "最新版本：${red} 失败跳过更新检查 手动更新${yellow} sudo $pip_install_cmd -U biliup${plain}"
+fi
+
+# 如果本地版本和最新版本不一致，提示用户更新
+if [ -n "$official_version" ] && [ "$local_version" != "$official_version" ]; then
+    read -p "本地版本和库中最新版本不一致，你希望更新biliup吗？[Y/N]：" update_choice
+    if [ -z "$update_choice" ]; then
+        update_choice=y
+    fi
+    if [ "$update_choice" = "y" ]; then
+        sudo $pip_install_cmd -U biliup
+        echo -e "更新后的版本是：${green} $official_version ${plain}"
+    else
+        echo -e "最新库中的版本是：${green} $official_version ${plain}"
+    fi
+fi
+    
+# 登录biliup-rs
+if [ ! -f "/opt/biliup/cookies.json" ]; then
+    read -p "未登录B站（cookier.json不存在）推荐使用扫码登录，是否登录？[Y/N]：" choice
+    if [ -z "$choice" ]; then
+        choice=y
+    fi
+    if [ "$choice" = "y" ]; then
+        sudo bash -c "/opt/biliup/biliupR login"
+        if [ -f "/opt/biliup/cookies.json" ]; then
+            echo -e "已从biliup-rs获取${yellow}cookie${red} 泄露会被盗登B站${plain}"
+        else
+            echo -e "未登录biliup-rs 请控制台手动执行 ${red}/opt/biliup/biliupR login${plain}"
+        fi
+    else
+        echo -e "cookie是登录B站所需 如上传请控制台手动执行 ${red}/opt/biliup/biliupR login${plain}"
+    fi
+fi
+
+# 禁用的端口列表
+ForbiddenPorts="0 1 7 9 11 13 15 17 19 20 21 22 23 25 37 42 43 53 77 79 87 95 101 102 103 104 109 110 111 113 115 117 119 123 135 139 143 179 389 465 512 513 514 515 526 530 531 532 540 556 563 587 601 636 993 995 2049 3659 4045 6000 6665 6666 6667 6668 6669 137 139 445 593 1025 2745 3127 6129 3389"
+
+# 互动输入端口
+while true; do
+    read -p  "请输入一个小于65535的端口(回车默认19159)： " UserPort
+    if [ -z "$UserPort" ]; then
+        UserPort=19159
+        echo -e "你使用了默认端口 ${green}$UserPort ${plain}  等待进入下一步"
+    fi
+    if [[ $UserPort =~ ^[0-9]+$ ]] && [ $UserPort -le 65535 ]; then
+        if [[ $ForbiddenPorts =~ (^|[[:space:]])$UserPort($|[[:space:]]) ]]; then
+            echo "错误: 端口 $UserPort 被禁用，请重新输入。"
+        elif lsof -i :$UserPort > /dev/null ; then
+            echo "错误: 端口 $UserPort 已被占用，请重新输入。"
+        else
+            if [ $UserPort != 19159 ]; then
+                echo -e "注意: 你选择的端口 ${yellow} $UserPort ${plain} 不是默认端口。如果你的防火墙设置阻止了该端口通信，请确保你已经在防火墙中打开了这个端口。"
+            fi
+            break
+        fi
+    else
+        echo "错误: 你输入的不是有效的端口号，请重新输入。"
+    fi
+done
+
+# 互动输入密码
+while true; do
+    read -r -p  "请输入密码(回车为不使用密码公网慎用）：" UserPassword
+    if [ -z "$UserPassword" ]; then
+        UserPassword=0
+        break
+    elif [[ "$UserPassword" =~ [$'\001'-$'\037'] ]]; then
+        echo "错误: 你输入的密码包含无效的字符，请重新输入。"
+    else
+        echo -e "账号：${green} biliup ${plain} 密码：${yellow} $UserPassword ${plain}"
+        break
+    fi
+done
+
+# 定义函数来运行biliup命令
+biliup_version=$(pip3 show biliup | grep Version | cut -d ' ' -f 2 | tr -d '.')
+run_biliup() {
+    if [[ $biliup_version -lt 0432 ]]; then
+        read -p  "0.4.32以下 是否开启hhtp？回车默认开启[Y/N]：" rrun
+        if [ -z "$rerun" ]; then
+            rrun=y
+        fi
+        if [ "$rrun" = "y" ]; then
+            echo -e "biliup v0.4.32以下 请到${red} /opt/biliup/config.toml ${plain}进行配置"
+            curl -L "https:///j.iokun.top/https://raw.githubusercontent.com/biliup/biliup/master/public/config.toml" -o "/opt/biliup/config.toml"
+            if [ "$UserPassword" = "0" ]; then
+                sudo bash -c "cd /opt/biliup && biliup --http -P $UserPort start"
+            else
+                sudo bash -c "cd /opt/biliup && biliup --http -P $UserPort --password '$UserPassword' start"
+            fi
+        else
+            echo -e "biliup v0.4.32以下 请到${red} /opt/biliup/config.toml ${plain}进行配置"
+            curl -L "https:///j.iokun.top/https://raw.githubusercontent.com/biliup/biliup/master/public/config.toml" -o "/opt/biliup/config.toml"
+            if [ "$UserPassword" = "0" ]; then
+                sudo bash -c "cd /opt/biliup && biliup -P $UserPort start"
+            else
+                sudo bash -c "cd /opt/biliup && biliup -P $UserPort --password '$UserPassword' start"
+            fi
+        fi   
+    else
+        if [ "$UserPassword" = "0" ]; then
+            sudo bash -c "cd /opt/biliup && biliup -P $UserPort start"
+        else
+            sudo bash -c "cd /opt/biliup && biliup -P $UserPort --password '$UserPassword' start"
+        fi   
+    fi
+}
+
+run_biliup
+
+# 检查biliup是否正在运行
+rm_biliup() {
+    if [ -n "$(curl -s ipinfo.io/ip)" ]; then    
+        echo -e "biliup已运行请至浏览器配置WEBUI  ${green}http://$(curl -s ipinfo.io/ip):$UserPort${plain}"
+    else
+        echo -e "biliup已运行请至浏览器配置WEBUI  ${green}http://[$(curl -s 6.ipw.cn)]:$UserPort${plain}"
+    fi
+    if [ -f "/opt/biliup/install.sh" ]
+    then
+        read -p  "你希望清理安装包吗？回车默认清理[Y/N]：" rerun
+        if [ -z "$rerun" ]; then
+            rerun=y
+        fi
+        if [ "$rerun" = "y" ]; then
+            rm -f /opt/biliup/biliupR-v0.1.19-*-linux.tar.xz
+            rm -rf /opt/biliup/biliupR-v0.1.19-*-linux
+            rm -f /opt/biliup/install.sh 
+            echo -e "${green}已清理安装包,biliu启动成功${plain}"
+        fi
+    fi
+}
+
+# 最后给用户一个提示
+if pgrep -f "biliup" > /dev/null; then
+    rm_biliup
+else
+    if [ -f "/opt/biliup/watch_process.pid" ]; then 
+        rm -f "/opt/biliup/watch_process.pid" ; run_biliup
+    else
+        run_biliup
+    fi
+    if ! pgrep -f "biliup" > /dev/null; then
+        echo $err_output
+        echo -e "${red}真一键biliup出问题了，请在QQ群中反馈${plain}"
+    else
+        rm_biliup
+    fi
+fi
