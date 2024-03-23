20a5690b9e31ad5034200cb1ded645dc8d857131
ikun1993 <96544807+ikun1993@users.noreply.github.com>
2024-03-23 15:59:12 +0800

    Update windowsbiliup.bat

--- windowsbiliup.bat
+++ windowsbiliup.bat
@@ -9,7 +9,7 @@ choco install ffmpeg -y
 choco install python310 -y
 
 :: Step 4: 添加 Python 路径到系统环境变量
-setx PATH "%PATH%;C:\Python310\Scripts\;C:\Python310\"
+setx PATH "%PATH%;C:\Python310\Scripts\;C:\Python310\" /M
 
 :: Step 5: 检测 IP 归属地并安装 biliup
 setlocal
@@ -18,17 +18,23 @@ REM 获取本机IP归属地
 for /f %%b in ('curl -s https://ipinfo.io/country') do (
     set CountryCode=%%b
 )
+for /f "tokens=2 delims= " %%i in ('yolk -V biliup 2^>nul') do set pipversion=%%i
+
+if not defined pipversion (
+    echo 查询最新版本失败，正在尝试安装 yolk3k...
+    powershell -Command "Start-Process -FilePath 'pip3' -ArgumentList 'install yolk3k' -Verb RunAs -Wait"
+    for /f "tokens=2 delims= " %%i in ('yolk -V biliup 2^>nul') do set pipversion=%%i
+    if not defined pipversion (
+        echo 检查库中版本失败 如需更新手动终端输入 pip3 install -U biliup ...
+        set pipversion=0.4.44
+    )
+) 
 echo IP归属地: "%CountryCode%"
 :: 判断 IP 归属地是否为中国
 if "%CountryCode%"=="CN" (
     echo 你的 IP 归属地是中国，将使用清华源安装 biliup。
-    pip install -i https://pypi.tuna.tsinghua.edu.cn/simple biliup==0.4.44
+    pip install -i https://pypi.tuna.tsinghua.edu.cn/simple biliup==%pipversion%
 ) else (
     echo 你的 IP 归属地不是中国，将使用默认源安装 biliup。
-    pip install biliup==0.4.44
+    pip install biliup==%pipversion%
 )
-
-
-endlocal
-:: 输出完成信息
-echo 已完成全部安装
